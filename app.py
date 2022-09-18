4ac303713270dcb39960057e2f56827f7b24493c
Ibeh Esther <estheribeh5@gmail.com>
2022-09-18 20:23:41 +0100

    refactor: Setup database connection, create POST and GET endpoints

--- app.py
+++ app.py
@@ -1,8 +1,10 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify, abort
 import requests
+from models import db, setup_db, News
 
 app = Flask(__name__)
 
+setup_db(app)
 BASE_URL = "https://hacker-news.firebaseio.com/v0"
 
 url = BASE_URL+"/item/32877585/.json"
@@ -19,9 +21,85 @@ def getFirst100RecentNews():
         top100RecentNews.append(response)
     return top100RecentNews
 
+@app.route("/news")
+def getNews():
+    body = []
+    try:
+        news = News.query.all()
+        for item in news:
+            news_details = {
+                "id": item.id,
+                "type": item.type,
+                "source": item.source,
+                "time": item.time,
+                "title": item.title,
+                "url": item.url,
+                "comments": item.comments
+            }
+            body.append(news_details)
+    except Exception:
+        abort(404)
+    return jsonify({"response": body})
+
+@app.route("/news", methods=["POST"])
+def add_news():
+    body = {}
+    try:
+        request = request.get_json()
+        type = request.get("type")
+        source = request.get("source")
+        time = request.get("time", 0)
+        title = request.get("title", "")
+        url = request.get("url", "")
+        comments = request.get("comments", [])
+        if(type and source):
+            news = News(type=type, source=source, time=time, 
+                        title=title, url=url, comments=comments)
+            db.session.add(news)
+            body["type"] = news.type
+            body["source"] = news.source
+            body["time"] = news.time
+            body["title"] = news.title
+            body["url"] = news.url
+            body["comments"] = news.comments
+            db.session.commit()
+        else:
+            abort(400)
+    except Exception:
+        db.session.rollback()
+        abort(400)
+    finally:
+        db.session.close()
+    return jsonify({"response": body})
+
 @app.route('/')
 def index():
     return render_template("index.html", stories= getFirst100RecentNews())
+
+# Error handlers
+@app.errorhandler(400)
+def bad_request(error):
+    return jsonify({
+        'success': False,
+        'error': 400,
+        'message': 'Bad Request'
+    }), 400
+
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        'success': False,
+        'error': 404,
+        'message': 'Not Found'
+    }), 404
+
+@app.errorhandler(405)
+def method_not_allowed(error):
+    return jsonify({
+        'success': False,
+        'error': 405,
+        'message': 'Method Not Allowed'
+    }), 405
 
 
 if(__name__ == "__main__"):
--- models.py
+++ models.py
@@ -1,21 +1,25 @@
 from flask_sqlalchemy import SQLAlchemy
-from app import app
 
+db = SQLAlchemy()
 # Connect to a cloud Postgres database
-app.config["SQLALCHEMY_DATABASE_URI"] = "postgres://yhkrulyb:E3SjR2v11GFmcwaPQocMF0PWHOVGRpIf@jelani.db.elephantsql.com/yhkrulyb"
-db = SQLAlchemy(app)
+def setup_db(app):
+    app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://yhkrulyb:E3SjR2v11GFmcwaPQocMF0PWHOVGRpIf@jelani.db.elephantsql.com/yhkrulyb"
+    app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+    db.app = app
+    db.init_app(app)
+    db.create_all()
 
 class News(db.Model):
     __tablename__ = "news"
     id = db.Column(db.Integer, primary_key = True)
-    type = db.Column(db.String(10), nullable=False)
+    type = db.Column(db.String(20), nullable=False)
+    source = db.Column(db.String(20), nullable=False)
     time = db.Column(db.Integer, nullable=True)
-    title = db.Column(db.String(10), nullable=True)
-    url = db.Column(db.String(10), nullable=True)
+    title = db.Column(db.String(120), nullable=True)
+    url = db.Column(db.String(120), nullable=True)
     comments = db.Column(db.ARRAY(db.Integer), nullable=True)
 
 
     def __repr__(self):
         return f'<News: id- {self.id} type- {self.type} title- {self.title} >'
 
-db.create_all()
